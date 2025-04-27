f5d0470df1bef89bc06bf6a67f822afaa54873ad
rovin001 <160404096+rovin001@users.noreply.github.com>
2025-04-27 16:15:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title="TradeScope AI", layout="wide")
 col1, col2 = st.columns([1, 8])
 
 with col1:
-    logo = Image.open("assets/escavic_logo.png")
+    logo = Image.open("assets/Escavic.jpg")
     st.image(logo, width=60)  # slightly smaller = cleaner
 
 with col2:
