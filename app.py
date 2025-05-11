961a2d9649d98c4184419051a9eb72cf42ac3982
rovin001 <160404096+rovin001@users.noreply.github.com>
2025-05-11 20:04:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ import streamlit as st
 #         show_cot_report()
 # elif tab == "🧠 News Summarizer":
 #     news_summarizer_tab()
-from pages import fundamental_data, sentiment_data, cot_data
+from pages import fundamentals, sentiment, news_summariser
 st.set_page_config(page_title="Escavic Dashboard", layout="wide")
 st.title("📊 Escavic – Forex Intelligence Dashboard")
 
